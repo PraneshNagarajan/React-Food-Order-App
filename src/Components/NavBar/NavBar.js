166e65ca45b7167094db4e36dc1d4cf536e61552
PraneshNagarajan <sathyajana20@gmail.com>
2021-06-03 21:47:30 +0530

    adding cart, modal

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,39 +1,76 @@
+import { Navbar, Badge, Container, Nav, Button,Form } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartPlus, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 
-import { Navbar, Badge,Container, Nav, Button} from "react-bootstrap";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faCartPlus, faSignOutAlt} from '@fortawesome/free-solid-svg-icons'
-
-const NavBar = () => {
+const NavBar = (props) => {
   return (
-    <Container fluid className="p-0">
-      <Navbar bg="primary" variant="dark" className="col-md-12">
-        <Navbar.Brand className="col-md-2">
-        <img src="https://img.icons8.com/fluent/40/000000/waiter.png" className="mx-3" />
-            ReactFood </Navbar.Brand>
-        <Nav className="col-md-4">
-          <Nav.Item>
+      <Navbar sticky="top" collapseOnSelect bg="primary" variant="dark" expand="md">
+              <Container fluid>
+              <Navbar.Brand>
+          <img
+            src="https://img.icons8.com/fluent/40/000000/waiter.png"
+            className="mr-2"
+          />
+          ReactMeals{" "}
+
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="#Navbar-collapse"></Navbar.Toggle>
+        <Navbar.Collapse id="Navbar-collapse">
+        <Nav className="ml-1 mr-auto">
+         <Nav.Item>
             <Nav.Link active>Home</Nav.Link>
           </Nav.Item>
           <Nav.Item>
             <Nav.Link>Blog</Nav.Link>
           </Nav.Item>
-          <Nav.Item>
+          <Nav.Item >
             <Nav.Link>About</Nav.Link>
           </Nav.Item>
-          <Nav.Item>
-         
-          </Nav.Item>
         </Nav>
-        <Navbar.Brand className="col-md-2 offset-1">
-        <img className="ml-5 mr-1" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4oIbrjiUXheYI1jpf9t9z7ip5Vep4NQ37kg&usqp=CAU" alt="user image" width="40px"  style={{borderRadius: `50%`, border: `1px solid white`}}/>
-        Pranesh Nagarajan
-        </Navbar.Brand>
-       <Nav className="col-md-3">
-       <Button  className="ml-5 mr-4" variant="outline-light"><FontAwesomeIcon icon={faCartPlus} className="mr-1"></FontAwesomeIcon>Orders<Badge className="ml-1" pill variant="warning">4</Badge></Button>
-       <Button  variant="outline-light">Logout <FontAwesomeIcon icon={faSignOutAlt}></FontAwesomeIcon></Button>
+        <Nav className="ml-1 mr-4">
+        <Nav.Item >
+          <Nav.Link active disabled>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4oIbrjiUXheYI1jpf9t9z7ip5Vep4NQ37kg&usqp=CAU"
+            alt="user image"
+            width="40px"
+            style={{ borderRadius: `50%`, border: `2px solid white` }}
+          ></img>
+          <span className="ml-2 h6">Pranesh Nagarajan</span>
+          </Nav.Link>
+          </Nav.Item>
        </Nav>
-      </Navbar>
-    </Container>
+      <Nav>
+        <Nav.Item className="m-1">
+        <Button
+        className="w-100"
+              variant="outline-light"
+              onClick={props.showFunction}
+            >
+              <FontAwesomeIcon
+                icon={faCartPlus}
+                className="mr-1"
+              ></FontAwesomeIcon>
+              Orders
+              <Badge className="ml-1" pill variant="warning">
+                4
+              </Badge>
+            </Button>
+        </Nav.Item>
+        <Nav.Item className="m-1">
+          
+        <Button 
+            className="w-100"
+            variant="outline-light">
+              Logout <FontAwesomeIcon icon={faSignOutAlt}></FontAwesomeIcon>
+            </Button>
+          
+        </Nav.Item>
+</Nav>
+        </Navbar.Collapse>
+        
+              </Container>
+        </Navbar>
   );
 };
 export default NavBar;
