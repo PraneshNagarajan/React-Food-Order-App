1946eed18b19bae66be33b6369eb8a246c4c69dc
PraneshNagarajan <sathyajana20@gmail.com>
2021-06-04 11:54:05 +0530

    adding context-api (or) useContext()

--- src/App.js
+++ src/App.js
@@ -2,11 +2,31 @@ import logo from "./logo.svg";
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import HomePage from './Pages/HomePage'
+import ItemsContext from "./Datas/Items-Context";
 function App() {
   return (
-    <div>
-      <HomePage/>
-    </div>
+    // If you pass dynamic value to components then only <Context.provider>. Otherwise there is no need.
+    //-------------------------------------------------------------------------------------------------
+      // <ItemsContext.Provider value ={[
+      //   {
+      //     item: "Sushi",
+      //     about: "Finest ish and veggie",
+      //     price: 25,
+      //     type: 'veg'
+      //   },
+      //   {
+      //     item: "Green Bowl",
+      //     about: "Greeny..and..Healthy",
+      //     price: 22.15,
+      //     type: 'veg'
+      //   }]}>
+      //   <HomePage/>
+      // </ItemsContext.Provider>
+
+
+      <div>
+        <HomePage/>
+      </div>
     );
 }
 
