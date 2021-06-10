d4b60760c99d109cfb6b40f722a5775ed78bd1b2
PraneshNagarajan <sathyajana20@gmail.com>
2021-06-10 15:30:52 +0530

    add React.memo()

--- src/UI/Meals/MealsInfo.js
+++ src/UI/Meals/MealsInfo.js
@@ -19,7 +19,15 @@ const MealsInfo = () => {
       </div>
     )
 }
-//memo - memorization , check previous value and current value is changed only this component would re-render, 
+// memo - memorization , check previous value and current value is changed only this component would re-render, 
 // e.g passing props but it doesn't change so, if parent component re-render based on state change. so child component would also re-render.
 // but, if child comp not get any new value. why re-render. it would be enhance performence.
+// but it doesn't work if pass function as props. because it's non-primitive(function, array). every time rerender pointer address get differ.
+// but boolean,string,num (primitive). it holds the value not memory location address.
+// if use memo in modal component doesn't work , because it has function as props. every time variable get re-initize of rendering
+//        <DialogModal
+//         datas={cartItems}
+//         show={isShow}
+//         showFunction={onShowModalHandler}
+//       />
 export default React.memo(MealsInfo)
