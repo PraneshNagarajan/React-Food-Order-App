c24589e641cd2ce604c61bc7ba9f316c7e50f784
PraneshNagarajan <sathyajana20@gmail.com>
2021-06-10 15:11:09 +0530

    add React.memo()

--- src/UI/Meals/MealsInfo.js
+++ src/UI/Meals/MealsInfo.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import {Card} from 'react-bootstrap'
 const MealsInfo = () => {
     return(
@@ -18,5 +19,7 @@ const MealsInfo = () => {
       </div>
     )
 }
-
-export default MealsInfo
+//memo - memorization , check previous value and current value is changed only this component would re-render, 
+// e.g passing props but it doesn't change so, if parent component re-render based on state change. so child component would also re-render.
+// but, if child comp not get any new value. why re-render. it would be enhance performence.
+export default React.memo(MealsInfo)
